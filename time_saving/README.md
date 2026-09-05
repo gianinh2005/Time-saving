14a6cc239e88ecea914d7ff4f0486d242beb605a
gianinh2005 <vogianinh250405@gmail.com>
2026-09-05 08:42:05 +0700

    Khởi tạo
    .

---
+++ time_saving/README.md
@@ -0,0 +1,3 @@
+# time_saving
+
+A new Flutter project.
---
+++ time_saving/android/app/src/main/kotlin/com/example/time_saving/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.time_saving
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
